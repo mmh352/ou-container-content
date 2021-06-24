7b0c664c657cb2ea85c26c2f4f81ed6d883ddb11
Mark Hall <mark.hall@open.ac.uk>
2021-06-24 16:05:05 +0100

    Test fix

--- tests/main_test/main_test.py
+++ tests/main_test/main_test.py
@@ -10,7 +10,7 @@ def test_basic_copy():
     """Test that the basic CLI works."""
     config_filename = os.path.join(os.getcwd(), os.path.dirname(__file__), 'config.yaml')
     runner = CliRunner()
-    result = runner.invoke(main, ['-c', config_filename])
+    result = runner.invoke(main, ['-c', config_filename, 'startup'])
     assert result.exit_code == 0
 
 
@@ -18,5 +18,5 @@ def test_fail_missing_config():
     """Test that a missing configuration file fails."""
     config_filename = os.path.join(os.getcwd(), os.path.dirname(__file__), 'missing-config.yaml')
     runner = CliRunner()
-    result = runner.invoke(main, ['-c', config_filename])
+    result = runner.invoke(main, ['-c', config_filename, 'startup'])
     assert result.exit_code == 2
